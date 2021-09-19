03378e147656f9445f94fd554f76a609825328a2
praveen291 <mudadlapraveenkumar143@gmail.com>
2021-09-19 11:54:34 +0530

    shrt way fil

--- praveen.js
+++ praveen.js
@@ -1,20 +1,20 @@
   //for each
 
-const company = [{
+const production = [{
   name: 'cmpny one', category: 'chips industry', start: 1981, end: 2021,
 
 },
 {
-  name: 'cmpny two', category: ' induction  industry', start: 1951, end: 2021,
+  name: 'cmpny two', category: 'chips industry', start: 1951, end: 2021,
 
 },
 {
-  name: 'cmpny one', category: 'tech industry', start: 1982, end: 2021,
+  name: 'cmpny three', category: 'publishers', start: 1982, end: 2021,
 
 },
 ];
 
-const kill= [33, 21, 23,]
+// const kill= [33, 21, 23,]
 
 
 // company.forEach((company) => {
@@ -35,5 +35,19 @@ const kill= [33, 21, 23,]
 
 
 //short way to
-const praveen = kill.filter(age => age >= 21);
- console.log(praveen);
+// const praveen = kill.filter(age => age >= 21);
+//  console.log(praveen);
+
+// filter retail company
+
+// const retailCompanies =  companies.filter( function(company)  {
+
+//   if (company.category === 'chips industry') {
+//     return true;
+//   }
+
+// });
+// console.log(retailCompanies);
+
+const praveen =production.filter(company => company.category === 'chips industry');
+console.log(praveen);
